a66c30f01ed105e4e629b2316368db47b7490552
LeeJaeSang <ljs91420@daum.net>
2024-08-30 16:00:53 +0900

    inquiryList.js 수정

--- src/main/resources/static/inquiry/js/inquiryList.js
+++ src/main/resources/static/inquiry/js/inquiryList.js
@@ -1,39 +1,61 @@
-const today = new Date();
-const year = today.getFullYear();
-const month = String(today.getMonth() + 1).padStart(2, '0');
-const day = String(today.getDate()).padStart(2, '0');
-const formattedToday = `${year}-${month}-${day}`;
-const formattedLastYear = year < 0 ? `${0}-${month}-${day}` : `${year - 1}-${month}-${day}`;
-
 $("#today-btn").click(() => {
-	$("#start-date").val(formattedToday);
-	$("#end-date").val(formattedToday);
-});
+	const today = new Date();
 
-$("#one-week-btn").click(() => {
-	const year = today.getFullYear();
-	const month = String(today.getMonth() + 1).padStart(2, '0');
-	const day = String(today.getDate()).padStart(2, '0');
-	
-	const formattedLastWeek = `${year}-${month}-${day}`;
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, '0');
+    const day = String(today.getDate()).padStart(2, '0');
+
+    const formattedDate = `${year}-${month}-${day}`;
 	
 	$("#start-date").val(formattedDate);
 	$("#end-date").val(formattedDate);
 });
 
-$("#one-month-btn").click(() => {
-	const year = today.getFullYear();
-	const month = String(today.getMonth() + 1).padStart(2, '0');
-	const day = String(today.getDate()).padStart(2, '0');
-	
+$("#one-week-btn").click(() => {	
+	const today = new Date();
+
+	const oneWeekAgo = new Date(today);
+	oneWeekAgo.setDate(today.getDate() - 7);
+
+	const year = oneWeekAgo.getFullYear();
+	const month = String(oneWeekAgo.getMonth() + 1).padStart(2, '0');
+	const day = String(oneWeekAgo.getDate()).padStart(2, '0');
+
 	const formattedDate = `${year}-${month}-${day}`;
 	
 	$("#start-date").val(formattedDate);
 	$("#end-date").val(formattedDate);
 });
 
+$("#one-month-btn").click(() => {
+	const today = new Date();
+
+	const oneMonthAgo = new Date(today);
+	oneMonthAgo.setMonth(today.getMonth() - 1);
+
+	const year = oneMonthAgo.getFullYear();
+	const month = String(oneMonthAgo.getMonth() + 1).padStart(2, '0');
+	const day = String(oneMonthAgo.getDate()).padStart(2, '0');
+
+	const formattedDate = `${year}-${month}-${day}`;
+
+	$("#start-date").val(formattedDate);
+	$("#end-date").val(formattedDate);
+});
+
 $("#one-year-btn").click(() => {
-	$("#start-date").val(formattedLastYear);
-	$("#end-date").val(formattedToday);
+	const today = new Date();
+	
+	const oneYearAgo = new Date(today);
+	oneYearAgo.setFullYear(today.getFullYear() - 1);
+
+	const year = oneYearAgo.getFullYear();
+	const month = String(oneYearAgo.getMonth() + 1).padStart(2, '0');
+	const day = String(oneYearAgo.getDate()).padStart(2, '0');
+
+	const formattedDate = `${year}-${month}-${day}`;
+	
+	$("#start-date").val(formattedDate);
+	$("#end-date").val(formattedDate);
 });
 
